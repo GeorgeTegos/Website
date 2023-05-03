8c951e77176a456c553fdac88130166c9c54b9d9
George Tegos <george.tegos.22@gmail.com>
2023-05-03 12:30:26 +0300

    Creation of FizzBuzz Game

---
+++ testing.js
@@ -0,0 +1,27 @@
+let x = prompt("FizzBuzz Game: \nGive a positive number")
+let count
+
+if(x == Number){
+    for (count=1;count <= x;count++){
+    if (((count%3) ==0) && ((count%5)==0)){
+        alert('FizzBuzz')
+    }
+    else if((count%5) == 0){
+        alert('Buzz')
+    }
+    else if((count%3) == 0){
+        alert('Fizz')
+    }
+    else{
+        alert(count)
+    }
+    }
+}
+else if(x == null || x == undefined || x.length<=0){
+    alert('Null Or Undefined')
+}
+else{
+    alert('Not a positive number')
+}
+
+
