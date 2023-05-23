486aa7d305db611e0256a580e66e0f88cbd39c04
George Tegos <george.tegos.22@gmail.com>
2023-05-23 17:31:39 +0300

    Creation of Games Section, insert FizzBuzz Game

--- testing.js
+++ testing.js
@@ -1,27 +1,29 @@
-let x = prompt("FizzBuzz Game: \nGive a positive number")
-let count
+function FizzBuzz(){
+    let x = Number(prompt("FizzBuzz Game: \nGive a positive number"))
+    let count
 
-if(x == Number){
-    for (count=1;count <= x;count++){
-    if (((count%3) ==0) && ((count%5)==0)){
-        alert('FizzBuzz')
+
+    if(x > 0 ){
+        for (count=1;count <= x;count++){
+        if (((count%3) ==0) && ((count%5)==0)){
+            alert('FizzBuzz')
+        }
+        else if((count%5) == 0){
+            alert('Buzz')
+        }
+        else if((count%3) == 0){
+            alert('Fizz')
+        }
+        else{
+            alert(count)
+        }
+        }
     }
-    else if((count%5) == 0){
-        alert('Buzz')
-    }
-    else if((count%3) == 0){
-        alert('Fizz')
+    else if(x == null || x == undefined || x.length<=0){
+        alert('Null Or Undefined')
     }
     else{
-        alert(count)
+        alert('Not a positive number')
     }
-    }
 }
-else if(x == null || x == undefined || x.length<=0){
-    alert('Null Or Undefined')
-}
-else{
-    alert('Not a positive number')
-}
-
 
